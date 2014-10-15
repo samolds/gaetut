8698d66c915db5028f8c2047b6fe24a5edf61352
Sam Olds <samolds@gmail.com>
2014-10-15 10:56:55 -0600

    moved static files up from sam to root

--- urls.py
+++ urls.py
@@ -6,4 +6,11 @@ handler500 = 'djangotoolbox.errorviews.server_error'
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/', }),
     (r'^/', include('sam.urls'))
+
+    #(r'^accounts/create_user/$', 'sam.views.create_new_user'),
+    #(r'^accounts/login/$', 'django.contrib.auth.views.login',
+    #    {'authentication_form': AuthenticationForm,
+    #    'template_name': 'sam/login.html',}),
+    (r'^admin/logout/$', 'django.contrib.auth.views.logout',
+        {'next_page': '/',}),
 )
