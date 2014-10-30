e54f18aeea18da84e52cc1369addef20e316e426
Sam Olds <samolds@gmail.com>
2014-10-30 15:01:09 -0600

    stripped out some unneccessary stuff and almost set the river page up

--- env/views/river.py
+++ env/views/river.py
@@ -1,8 +1,7 @@
-from django.views.decorators.cache import never_cache
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
-from sam.models import Post
+#from env.models import Post
 from django.db.models import Q
 from django.core.cache import cache
 from TwitterAPI import TwitterAPI
@@ -11,7 +10,6 @@ from datetime import datetime, timedelta
 import simplejson as json
 import soundcloud
 import flickrapi
-#import facebook
 import markdown
 import github3
 import oauth2
@@ -38,33 +36,31 @@ def embeds(embeds):
         if (embed['type'] == "lastfm" and embed['nowplaying']):
             embed['date'] = 0
 
-    return HttpResponse(json.dumps({
-      "embeds": embeds,
-      "USERNAME": settings.USERNAME}), content_type="application/json")
+    return HttpResponse(json.dumps(embeds), content_type="application/json")
 
 
 # BLOGS
-def blogs(request):
-    blogs = cache.get("blogs")
-    if not blogs:
-        blogs = []
-        try:
-            public = Q(private=False)
-            blog_posts = Post.objects.order_by('-creation_date').filter(public)
-            if len(blog_posts) > 5:
-                blog_posts = blog_posts[:5]
-            for blog in blog_posts:
-                blogs.append({
-                    "type": "blog",
-                    "date": blog.creation_date,
-                    "link": "/blog/post/%d" % blog.pk,
-                    "title": blog.title,
-                    "stub": blog.small_stub,
-                })
-            cache.set("blogs", blogs, 60 * 60)
-        except:
-            pass
-    return embeds(blogs)
+#def blogs(request):
+#    blogs = cache.get("blogs")
+#    if not blogs:
+#        blogs = []
+#        try:
+#            public = Q(private=False)
+#            blog_posts = Post.objects.order_by('-creation_date').filter(public)
+#            if len(blog_posts) > 5:
+#                blog_posts = blog_posts[:5]
+#            for blog in blog_posts:
+#                blogs.append({
+#                    "type": "blog",
+#                    "date": blog.creation_date,
+#                    "link": "/blog/post/%d" % blog.pk,
+#                    "title": blog.title,
+#                    "stub": blog.small_stub,
+#                })
+#            cache.set("blogs", blogs, 60 * 60)
+#        except:
+#            pass
+#    return embeds(blogs)
 
 
 # TWITTER
@@ -125,7 +121,7 @@ def lastfms(request):
             consumer = oauth2.Consumer(key=lastfmkey, secret=lastfmsecret)
             client = oauth2.Client(consumer)
 
-            url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + settings.USERNAME + "&format=json&limit=%d&api_key=%s" % (29, lastfmkey)
+            url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + lfm['USERNAME'] + "&format=json&limit=%d&api_key=%s" % (29, lastfmkey)
 
             resp, content = client.request(url, 'GET')
             lastfmtracks = json.loads(content)['recenttracks']['track']
@@ -138,7 +134,7 @@ def lastfms(request):
                 else:
                     date = datetime.strptime(track['date']['#text'], '%d %b %Y, %H:%M')
                     track.update({"nowplaying": False})
-                track.update({"artist": track['artist']['#text'], "date": date, "type": "lastfm"})
+                track.update({"artist": track['artist']['#text'], "date": date, "type": "lastfm", "USERNAME": lfm['USERNAME']})
             cache.set("lastfms", lastfmtracks, 60 * 1)
         except:
             pass
@@ -152,6 +148,7 @@ def flickrs(request):
         flickrs = []
         try:
             f = settings.FLICKR
+            usr = f['USERNAME']
             flickr = flickrapi.FlickrAPI(f['KEY'])
             all_photos = flickr.photos_search(user_id=f['USER_ID'], per_page='5')
             for photos in all_photos:
@@ -165,7 +162,7 @@ def flickrs(request):
                     description = markdown.markdown(info.photo[0].description[0].text)
                     title = info.photo[0].title[0].text
                     photolink = "http://farm%s.staticflickr.com/%s/%s_%s_n.jpg" % (farm, s_id, p_id, p_secret)
-                    flickrs.append({"imgsrc": photolink, "photoid": p_id, "date": p_date, "type": "flickr", "description": description, "title": title})
+                    flickrs.append({"imgsrc": photolink, "photoid": p_id, "date": p_date, "type": "flickr", "description": description, "title": title, "USERNAME": usr})
             cache.set("flickrs", flickrs, 60 * 60 * 12)
         except:
             pass
@@ -178,7 +175,8 @@ def githubs(request):
     if not githubs:
         githubs = []
         try:
-            me = github3.user(settings.USERNAME)
+            gh = settings.GITHUB
+            me = github3.user(gh['USERNAME'])
             consumer = oauth2.Consumer(key="", secret="")
             client = oauth2.Client(consumer)
             resp, content = client.request(me.events_urlt.expand(), 'GET')
@@ -188,74 +186,9 @@ def githubs(request):
                 date = datetime.strptime(event['created_at'], "%Y-%m-%dT%H:%M:%SZ")
                 e_type = event['type']
                 git_event.update({"repo_url": "http://github.com/%s" % (event['repo']['name'])})
-                git_event.update({"date": date, "type": "github", "event_type": e_type})
+                git_event.update({"date": date, "type": "github", "event_type": e_type, "USERNAME": gh['USERNAME']})
                 githubs.append(git_event)
             cache.set("githubs", githubs, 60 * 60)
         except:
             pass
     return embeds(githubs)
-
-
-
-# IMGUR
-def imgurs(request):
-    imgurs = cache.get("imgurs")
-    if not imgurs:
-        imgurs = []
-        try:
-            i_client = settings.IMGUR['CLIENT_ID']
-            i_secret = settings.IMGUR['SECRET']
-            i_code = settings.IMGUR['ACCESS_CODE']
-            i_username = settings.USERNAME
-
-            consumer = oauth2.Consumer(key='', secret='')
-            client = oauth2.Client(consumer)
-            
-            toGetCodeUrl = "https://api.imgur.com/oauth2/authorize?client_id=%s&response_type=token" % i_client
-
-            params = "client_id=%s&client_secret=%s&grant_type=%s&code=%s" % (i_client, i_secret, "authorization_code", i_code)
-            resp, content = client.request('https://api.imgur.com/oauth2/token', method='POST', body=params)
-            token = json.loads(content)['access_token']
-
-            #x, y = client.request(toGetCodeUrl, method='GET')
-            #import pdb; pdb.set_trace()
-
-            url = "https://api.imgur.com/3/account/%s/favorites" % i_username
-            resp, content = client.request(url, 'GET', headers={"Authorization": "Bearer %s" %token})
-
-            favorites = json.loads(content)['data']
-            for favorite in favorites:
-                uploaded_at = datetime.fromtimestamp(int(favorite['datetime']))
-                imgur = {"title": favorite['title'], "date": uploaded_at, "type": "imgur", "is_album": favorite['is_album']}
-                if favorite['is_album']:
-                    imgur.update({"imgsrc": "http://imgur.com/%s.gif" % favorite['cover'], "link": favorite['link']})
-                else:
-                    imgur.update({"imgsrc": favorite['link'], "link": "http://imgur.com/%s" % favorite['id']})
-                imgurs.append(imgur)
-            cache.set("imgurs", imgurs, 60 * 60)
-        except:
-            pass
-    return embeds(imgurs)
-
-
-# FACEBOOK - ABANDON...
-def facebooks(request):
-    facebooks = cache.get("facebooks")
-    if not facebooks:
-        facebooks = []
-        try:
-            key = ""
-            secret = ""
-            # short_token = just taken user token and figure out what to do, even though it expires...
-            # token = "https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s" % (key, secret, short_token)
-            # graph = facebook.GraphAPI(token)
-            # me = graph.get_object("me")
-
-            #key = 
-            #secret = 
-            #for x in y:
-            #githubs.append({"imgsrc": photolink, "photoid": p_id, "date": p_date, "type": "flickr", "description": description, "title": title})
-            #cache.set("facebooks", facebooks, 60 * 60)
-        except:
-            pass
-    return embeds(facebooks)
