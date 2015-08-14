8f5d5f7ff6010766ecba2a189f76de6d7fb654f3
Sam Olds <samolds@gmail.com>
2015-08-14 12:22:33 -0600

    cleaned up checkpoint 4 to be less of a mess and flow better into cp5

--- checkpoints/4-interactive/controllers/greet.py
+++ checkpoints/4-interactive/controllers/greet.py
@@ -1,8 +1,5 @@
-from google.appengine.api import users
 from controllers.base import BaseRequestHandler
-import urllib
-
-from models import DEFAULT_GUESTBOOK_NAME
+from models import DB_NAME
 from models import guestbook_key
 from models import Author
 from models import Greeting
@@ -10,51 +7,31 @@ from models import Greeting
 
 class Greet(BaseRequestHandler):
   def get(self):
-    guestbook_name = self.request.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
-    greetings_query = Greeting.query(ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
-    greetings_obj = greetings_query.fetch(10)
+    query = Greeting.query(ancestor=guestbook_key(DB_NAME)).order(-Greeting.date)
+    greetings_obj = query.fetch(10)
 
-    user = users.get_current_user()
     greetings = []
     for greeting in greetings_obj:
-      if greeting.author:
-        if user and user.user_id() == greeting.author.identity:
-          author = greeting.author.email + ' (You)'
-        else:
-          author = greeting.author.email
+      if greeting.author and greeting.author.name:
+        author = greeting.author.name
+      elif greeting.author and greeting.author.email:
+        author = greeting.author.email
       else:
-        author = "An anonymous person"
+        author = "Anonymous"
+
       greetings.append({
         "author": author,
         "content": greeting.content,
       })
 
-    if user:
-      url = users.create_logout_url(self.request.uri)
-      url_linktext = 'Logout'
-    else:
-      url = users.create_login_url(self.request.uri)
-      url_linktext = 'Login'
-
-    self.generate("greet.html", {
-      "greetings": greetings,
-      "guestbook_name": guestbook_name,
-      "url": url,
-      "url_linktext": url_linktext,
-    })
+    self.generate("greet.html", {"greetings": greetings})
 
 
   def post(self):
-    guestbook_name = self.request.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
-    greeting = Greeting(parent=guestbook_key(guestbook_name))
-
-    if users.get_current_user():
-      greeting.author = Author(
-          identity=users.get_current_user().user_id(),
-          email=users.get_current_user().email())
-
+    greeting = Greeting(parent=guestbook_key(DB_NAME))
+    user_name = self.request.get('name')
+    user_email = self.request.get('email')
+    greeting.author = Author(name=user_name, email=user_email)
     greeting.content = self.request.get('content')
     greeting.put()
-
-    query_params = {'guestbook_name': guestbook_name}
-    self.redirect('/greet?' + urllib.urlencode(query_params))
+    self.redirect('/greet')
--- checkpoints/4-interactive/models.py
+++ checkpoints/4-interactive/models.py
@@ -1,29 +1,23 @@
 from google.appengine.ext import ndb
+import settings
 
 
-DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
+try:
+  DB_NAME = settings.DB_NAME
+except AttributeError:
+  DB_NAME = 'dev-guestbook'
 
 
-# We set a parent key on the 'Greetings' to ensure that they are all
-# in the same entity group. Queries across the single entity group
-# will be consistent.  However, the write rate should be limited to
-# ~1/second.
-
-def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
-  """Constructs a Datastore key for a Guestbook entity.
-  We use guestbook_name as the key.
-  """
+def guestbook_key(guestbook_name=DB_NAME):
   return ndb.Key('Guestbook', guestbook_name)
 
 
 class Author(ndb.Model):
-  """Sub model for representing an author."""
-  identity = ndb.StringProperty(indexed=False)
+  name = ndb.StringProperty(indexed=False)
   email = ndb.StringProperty(indexed=False)
 
 
 class Greeting(ndb.Model):
-  """A main model for representing an individual Guestbook entry."""
   author = ndb.StructuredProperty(Author)
   content = ndb.StringProperty(indexed=False)
   date = ndb.DateTimeProperty(auto_now_add=True)
